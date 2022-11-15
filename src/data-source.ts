fe2fd62982dae29573a7efe4e97931f4f22751b6
NaianeReis27 <email.annereis@gmail.com>
2022-11-15 14:32:14 -0300

    teste

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,18 +2,30 @@ import { DataSource } from "typeorm";
 import "dotenv/config";
 
 const AppDataSource = new DataSource(
-     {
-        type: "postgres",
-        host: process.env.HOST,
-        port: 5432,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        logging: true,
-        synchronize: false,
-        entities: ["src/entities/*.ts"],
-        migrations: ["src/migrations/*.ts"],
-      }
+  {
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+    synchronize: false,
+    logging: true,
+    entities: process.env.NODE_ENV === "production" ? ["dist/src/entities/*.js"] : ["src/entities/*.ts"],
+    migrations: process.env.NODE_ENV === "production" ? ["dist/src/migrations/*.js"] : ["src/migrations/*.ts"],
+  }
+  
+  
+    //  {
+    //     type: "postgres",
+    //     host: process.env.HOST,
+    //     port: 5432,
+    //     username: process.env.POSTGRES_USER,
+    //     password: process.env.POSTGRES_PASSWORD,
+    //     database: process.env.POSTGRES_DB,
+    //     logging: true,
+    //     synchronize: false,
+    //     entities: ["src/entities/*.ts"],
+    //     migrations: ["src/migrations/*.ts"],
+    //   }
 );
+
 
 export default AppDataSource;
